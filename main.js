8c4641f6fcecfdba70064589d3ae79452f1f272c
Tar-Nway-Oo <tarnwayo@gmail.com>
2023-12-08 15:19:38 +0630

    create removeCard function and css media query

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const warning = document.querySelector(".warning");
 const cardNumber = document.querySelector(".card-number");
 const clearBtn = document.querySelector("#clear");
 let cards = [];
+let id = 1;
 
 function addCard(e) {
    e.preventDefault();
@@ -14,32 +15,46 @@ function addCard(e) {
       return;
    }
    warning.textContent = "";
-   const cardObject = {frontSide: front.value, backSide: back.value};
+   const cardObject = {id: id, frontSide: front.value, backSide: back.value};
    cards.push(cardObject);
-   createCard();
+   id = id + 1;
+   createCard(cards);
 }
-
-function createCard() {
-      const card = cards[cards.length - 1];
+function createCard(cardArray) {
+      const card = cardArray[cardArray.length - 1];
       const cardElement = document.createElement("div");
       cardElement.classList.add("card")
       cardElement.addEventListener("click", flipCard);
       
       const frontEl = document.createElement("p");
       const backEl = document.createElement("p");
+      const removeIcon = document.createElement("img");
+
       frontEl.textContent = card.frontSide;
       frontEl.classList.add("card-front");
       backEl.textContent = card.backSide;
       backEl.classList.add("card-back");
+      removeIcon.setAttribute("src", "./images/delete-icon.png");
+      removeIcon.classList.add("card-front");
+      removeIcon.classList.add("remove-icon");
+      removeIcon.addEventListener("click", () => {removeCard(cardElement, card.id)});
 
       cardElement.appendChild(frontEl);
       cardElement.appendChild(backEl);
+      cardElement.appendChild(removeIcon);
       cardContainer.appendChild(cardElement);
-      cardNumber.textContent = cards.length ;
+      cardNumber.textContent = cardArray.length ;
       front.value = "";
       back.value = "";
 
 }
+
+function removeCard(cardElement, id) {
+  cardContainer.removeChild(cardElement);
+  cards = cards.filter(card => card.id !== id);
+  cardNumber.textContent = cards.length;
+}
+
 
 function flipCard({target}) {
    target.classList.toggle("flipped");
